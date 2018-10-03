21e68d5abb9205099d78df729434e21ac8c4320a
Mac <denpef@gmail.com>
2018-10-04 02:22:18 +0300

    Initial Commit

---
+++ Ticker/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Ticker
+//
+//  Created by Денис Ефимов on 04.10.2018.
+//  Copyright © 2018 Denis Efimov. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
